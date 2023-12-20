2c504b2618cf3daafe2832cc2f34d0395318ddec
unknown <octamarina@gmail.com>
2023-12-20 23:40:29 +0200

    Primul commit

---
+++ OpenGL-CLion-Template/cmake-build-debug-visual-studio/CMakeFiles/OpenGL_CLion_Template.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Octa/Desktop/Projects/opengl/templates/simpleapp/OpenGL-CLion-Template/libraries/glad/src/glad.c" "CMakeFiles/OpenGL_CLion_Template.dir/libraries/glad/src/glad.c.obj" "msvc" "CMakeFiles/OpenGL_CLion_Template.dir/libraries/glad/src/glad.c.obj.d"
+  "C:/Users/Octa/Desktop/Projects/opengl/templates/simpleapp/OpenGL-CLion-Template/src/main.cpp" "CMakeFiles/OpenGL_CLion_Template.dir/src/main.cpp.obj" "msvc" "CMakeFiles/OpenGL_CLion_Template.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenGL-CLion-Template/cmake-build-debug-visual-studio/CMakeFiles/OpenGL_CLion_Template.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_CLion_Template.dir/libraries/glad/src/glad.c.obj"
+  "CMakeFiles/OpenGL_CLion_Template.dir/libraries/glad/src/glad.c.obj.d"
+  "CMakeFiles/OpenGL_CLion_Template.dir/src/main.cpp.obj"
+  "CMakeFiles/OpenGL_CLion_Template.dir/src/main.cpp.obj.d"
+  "OpenGL_CLion_Template.exe"
+  "OpenGL_CLion_Template.exe.manifest"
+  "OpenGL_CLion_Template.lib"
+  "OpenGL_CLion_Template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL_CLion_Template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
